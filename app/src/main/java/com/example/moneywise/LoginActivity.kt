3fb9bf2ba89750be46a753c96775a747d21de583
Tamanna <tamannafzr@gmail.com>
2023-11-18 14:52:06 -0500

    added code for login UI for both indiviual and admin user Created logo for app and added i to landing page

--- app/src/main/java/com/example/moneywise/LoginActivity.kt
+++ app/src/main/java/com/example/moneywise/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.example.moneywise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -10,16 +14,30 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
     // Firebase access
     private lateinit var firebaseAuth: FirebaseAuth
-
+    lateinit var signInButton: Button
+    lateinit var signUpButton: Button
     private val TAG = "Auth: Login"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login_indiviual)
+        signInButton = findViewById(R.id.btn_signInInd)
+        signUpButton = findViewById(R.id.btn_signUp)
+        signInButton.setOnClickListener {
+            val indUserId = findViewById<EditText>(R.id.edit_usernameInd).text.toString()
+            val password = findViewById<EditText>(R.id.edit_passwordInd).text.toString()
 
-        // Init firebase
+            // Init firebase
         firebaseAuth = Firebase.auth
 
         // Set listener(s)
+    }
+
+        signUpButton.setOnClickListener {
+            Log.i("TAG", "SignUp Button  is  clicked!")
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Navigating to SignUp View", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun loginUser() {
@@ -34,4 +52,6 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+
+
 }
