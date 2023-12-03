7f280519afafd38dd2712d0ce69b5db9f6af2cf1
JLaxon <jlaxon@my.centennialcollege.ca>
2023-12-03 13:45:29 -0500

    Added Home Page, reporting, budgeting, receipt scanner, and settings page

---
+++ app/src/main/java/com/example/moneywise/ReportingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneywise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ReportingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reporting)
+
+    }
+}
---
+++ app/src/main/java/com/example/moneywise/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneywise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SettingsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+    }
+}
