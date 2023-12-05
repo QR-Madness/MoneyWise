c6225568905c8b8660ef49fa26c0c7cb2c0190c5
Tamanna <tamannafzr@gmail.com>
2023-12-05 14:14:24 -0500

    Added code for xml files and added UI for all pages

---
+++ app/src/main/java/com/example/moneywise/ExpenseManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moneywise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExpenseManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense)
+    }
+}
---
+++ app/src/main/java/com/example/moneywise/ReportingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moneywise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reporting)
+    }
+}
